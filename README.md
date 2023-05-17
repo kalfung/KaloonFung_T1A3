7f973bde1fa3e14c27223167841f420c2dbb098a
kalfung <14322@coderacademy.edu.au>
2023-05-17 12:54:17 +1000

    created loop for random encounters

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # KaloonFung_T1A3
 Terminal application
+
+# (R7) Implementation Plan
+
+[Implementatiopn Plan on Trello](https://trello.com/b/38r4jreP/t1a3-terminal-application)
---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+import sys
+while True:
+    decision = input('COntinue to the next area? (y/n): ').lower()
+    if decision == 'n':
+        print('Until next time!')
+        sys.exit()
+    elif decision == 'y':
+        encounter = random.randint(1,10)
+        if encounter >= 9:
+            print(f'{encounter} You discover a treasure coffer!')
+        elif encounter >= 3:
+            print(f'{encounter} You run into a monster!')
+        else:
+            print(f'{encounter} You encounter a treasure trap!')
+    else:
+        print('Can\'t do that. Try again!')
