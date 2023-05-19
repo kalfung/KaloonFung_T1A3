623f792ba79e1f8b05292167ab7cadff7807fa3e
kalfung <14322@coderacademy.edu.au>
2023-05-19 19:57:44 +1000

    removed objects.py

--- objects.py
+++
@@ -1,9 +0,0 @@
-class Player:
-    def __init__(self, name, maxhealth, attack, cure, gold):
-        self.name = name
-        self.maxhealth = maxhealth
-        self.health = maxhealth
-        self.attack = attack
-        self.cure = cure
-        self.gold = gold
-        self.alive = True
